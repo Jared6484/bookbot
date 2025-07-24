3c253f13d4dfef49e4adabfa57ad43a262b86600
jared6484 <jared.6484@gmail.com>
2025-07-23 23:59:20 -0400

    B: Final Version

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from stats import get_word_count, get_char_count, sort_dict
+import sys
 
 def get_book_text(f): 
     file_contents = f.read()
     return file_contents
 
 def main():
-    path_to_book = "books/frankenstein.txt"
-    with open(path_to_book) as f:
+    #path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    with open(book) as f:
         words = get_book_text(f)
     num_words = get_word_count(words)
     char_count_dict = get_char_count(words)  # rtn's a dict of {'char' : int}
     sorted_list = sort_dict(char_count_dict)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book}...")
 
 
     print("----------- Word Count ----------")
